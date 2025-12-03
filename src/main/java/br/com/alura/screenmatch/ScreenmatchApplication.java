cb82a767f30a19c30d6b8d8129e07c259d3dc5f2
Luiz <luizzlh2004@gmail.com>
2025-12-02 22:20:39 -0300

    Adicionado classe record de dados do episodio e da temporada

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.models.DadosEpisodio;
 import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.models.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,8 +23,25 @@ public class ScreenmatchApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         var consumoAPI = new ConsumoAPI();
         var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=35ffd357");
+
         System.out.println(json);
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=1&apikey=35ffd357");
+        DadosEpisodio episodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(episodio);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 0; i <= dados.totalTemporadas(); i++){
+            json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i +"&apikey=35ffd357");
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
+
     }
 }
---
+++ src/main/java/br/com/alura/screenmatch/models/DadosEpisodio.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.models;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Episode") Integer numero,
+                            @JsonAlias("Released") String dataLancamento,
+                            @JsonAlias("imdbRating") String avaliacao) {
+}
